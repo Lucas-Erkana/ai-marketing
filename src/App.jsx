47e96b8179cdcefe209830ab90bd02d26ec48490
Lucas David Erkana <lderkana21x@gmail.com>
2023-12-12 21:53:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      Hello
-    </div>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add more Route components here for additional pages */}
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
