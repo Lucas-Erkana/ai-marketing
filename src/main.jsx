1944e2882dc4621f4fb7948a67bcc5a0387afe07
Lucas David Erkana <lderkana21x@gmail.com>
2023-12-12 19:35:41 +0200

    create react app

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
